ae2289472a08cb044b6806c673634d6258f3f4b6
Franz Medrano <franz.medrano@icloud.com>
2025-12-08 23:10:17 -0400

    10:payments

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from "@/app/features/auth/components/register-form";
+import { RegisterForm } from "@/features/auth/components/register-form";
 import { requireUnAuth } from "@/lib/auth-utils";
 
 const Page = async () => {
