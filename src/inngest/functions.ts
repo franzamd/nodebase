bb06273dfa2114b34ff0fa33ee6036e06b90df19
Franz Medrano <franz.medrano@icloud.com>
2026-01-01 13:44:15 -0400

    21: node realtime

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,11 +4,19 @@ import prisma from "@/lib/db";
 import { topologicalSort } from "./utils";
 import { NodeType } from "@/generated/prisma/enums";
 import { getExecutor } from "@/features/executions/lib/executor-registry";
+import { httpRequestChannel } from "./channels/http-request";
+import { manualTriggerChannel } from "./channels/manual-trigger";
 
 export const executeWorkflow = inngest.createFunction(
-  { id: "execute-workflow" },
-  { event: "workflows/execute.workflow" },
-  async ({ event, step }) => {
+  {
+    id: "execute-workflow",
+    retries: 0, // TODO: REMOVE IN PRODUCTION
+  },
+  {
+    event: "workflows/execute.workflow",
+    channles: [httpRequestChannel(), manualTriggerChannel()],
+  },
+  async ({ event, step, publish }) => {
     const workflowId = event.data.workflowId;
 
     if (!workflowId) {
@@ -37,6 +45,7 @@ export const executeWorkflow = inngest.createFunction(
         nodeId: node.id,
         context,
         step,
+        publish,
       });
     }
 
